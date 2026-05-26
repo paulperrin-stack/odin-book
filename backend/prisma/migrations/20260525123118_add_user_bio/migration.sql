b93f52ffd1ac2a520a9802798aa22e3e1f7ba792
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-05-26 16:13:32 +0700

    feat(db): add bio field to User model

---
+++ backend/prisma/migrations/20260525123118_add_user_bio/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrl` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "imageUrl";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "bio" TEXT;
